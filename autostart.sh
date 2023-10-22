ce0a091d1c01a70a269b18cd6c3126f287a55c98
Dronne34 <ozy23as@gmail.com>
2023-10-22 06:37:56 +0300

    update autostart

--- autostart.sh
+++ autostart.sh
@@ -4,6 +4,9 @@ exec nitrogen --restore &
 exec xrandr -s 1920x1080 &
 exec picom --config  ~/.config/picom/picom.conf  &
 exec /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+exec xrdb -merge ~/.Xresources  &
+exec chmod u+x .local/bin/* &
+exec eval "$(ssh-agent -s)"  &
 exec sxhkd &    
 exec dunst &
 
